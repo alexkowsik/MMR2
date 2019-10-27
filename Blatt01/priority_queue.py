1e7acb186a20cbcc1587dcc261a797f7aedd4c15
Alexander Kowsik <alexander.kowsik@gmx.de>
2019-10-27 21:54:19 +0100

    simple priority queue

---
+++ Blatt01/priority_queue.py
@@ -0,0 +1,16 @@
+class PQueue:
+    def __init__(self):
+        self.items = []
+
+    def push(self, u, value):
+        self.items.append((u, value))
+        self.items.sort()
+
+    def decrease_key(self, u, value):
+        for index, element in enumerate(self.items):
+            if element[0] == u:
+                self.items[index][1] = value
+                return
+
+    def pop_min(self):
+        return self.items.pop(0)
