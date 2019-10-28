26a5f3b398840bb86b2a272113756b55c73a7acd
Alexander Kowsik <alexander.kowsik@gmx.de>
2019-10-28 18:20:41 +0100

    dijkstra finished. needs testing

--- assignment01/dijkstra.py
+++ assignment01/dijkstra.py
@@ -1,31 +1,44 @@
 from MMR2.assignment01.priority_queue import PQueue
-from MMR2.assignment01.graph import GraphAsMatrix, GraphAsList
 
-graph = GraphAsList()  # create graph
 
-s = None  # start node (u, value)
-t = None  # end node (u, value)
+def dijkstra(graph):
+    s = None  # start node (id, value, BG, LG)
+    t = None  # end node (id, value, BG, LG)
 
-G = PQueue()
-S = list()
-p = PQueue()
-d = PQueue()  # push nodes with float("inf") values?
+    G = PQueue()
+    S = list()
+    p = [None] * graph.numOfNodes
+    #d = [float("inf")] * graph.numOfNodes
 
-G.push(s[0], s[1])
-d.push(s, 0)
+    s[1] = 0  # d_u is now in value of u
+    G.push(s)
 
-while G.get_length():
-    u = G.pop_min()
-    S.append(u)
+    while G.get_length():
+        u = G.pop_min()
+        S.append(u)
 
-    if u == t:
-        break
-
-    out_edges = graph.out_edges(u)
-
-    for edge in out_edges:
-        if edge[1] == u:
+        if u == t:
             break
-        if not edge[0] in G:
-            G.push(edge[0])
-            # ...
+
+        out_edges = graph.out_edges(u)
+
+        for edge in out_edges:
+            if edge[1] in S:
+                break
+            if not edge[1] in G:
+                edge[1][1] = edge[0][1] + edge[2]  # value of v = value of u + weight of edge
+                G.push(edge[1])
+                p[edge[1][0]] = edge
+            elif edge[0][1] + edge[2] < edge[1][1]:
+                edge[1][1] = edge[0][1] + edge[2]
+                p[edge[1][0]] = edge
+
+    if t[1] == float("inf"):
+        return False
+
+    P = list()
+    u = t
+
+    while u != s:
+        P.append(p[u[0]])
+        u = p[u[0]][0]
