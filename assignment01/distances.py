862a315dac33c5678c5f7b42c74b77a723284be2
DannyD <bittenameeinfugen@gmail.com>
2019-10-28 21:57:24 +0100

    task 2 works now with dictionaries, but pyqt5 drawing needs to be worked on

--- assignment01/distances.py
+++ assignment01/distances.py
@@ -1,59 +1,100 @@
-from graph import GraphAsList
-from math import sin,cos,pi,acos
-
-g = GraphAsList()
+from PyQt5 import QtGui
+from PyQt5.QtCore import Qt,QPoint
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from math import sin,cos,acos,sqrt,pow
+import sys
+from graph import GraphAsList, GraphAsDict
+import csv
+g = GraphAsDict()
 with open("maps/nodes.csv",newline="") as file:    #open file
     reader = csv.reader(file,delimiter=",")
     for row in reader:                                  #returns rows
         g.add_node(int(row[0]),0 ,float(row[2]),float(row[1]))        #row[0]: node,[1]:breitengrad,[2]:laengengrad
 
-counter = 0
-rowCount = 0
-#iterators sind rotz
 with open("maps/edges.csv", newline="") as file:  # open file
-    lis = [line.split() for line in file]
-    lis = [row[0].split(',') for row in lis]
-    while rowCount < len(lis)-1:
-        #manual insertion of edges.
-        tmp = g.nodes[counter]
-        beta1 = tmp[3]
-        lambda1 = tmp[4]
-        tmp = next(x for x in g.nodes if x[0] == int(lis[rowCount][1])) #next findet den node, zu dem die kante geht
-        beta2 = tmp[3]
-        lambda2 = tmp[4]
+    reader = csv.reader(file,delimiter= ",")
+    for row in reader:
+        beta1 =g.nodes[int(row[0])][2]
+        lambda1=g.nodes[int(row[0])][3]
+        beta2=g.nodes[int(row[1])][2]
+        lambda2=g.nodes[int(row[1])][3]
         dist = 6378.388 * acos(sin(beta1) * sin(beta2) + cos(beta1) * cos(beta2) * cos(abs(lambda1 - lambda2)))
-        g.nodes[counter][2].append((int(lis[rowCount][1]),dist))
-        if lis[rowCount+1][0] != lis[rowCount][0]:
-            counter += 1
-        rowCount +=1
-        if rowCount % 10000 == 0:
-            print(rowCount/1600.0,"%")
-    #add the last edge
-    tmp = g.nodes[counter]
-    beta1 = tmp[3]
-    lambda1 = tmp[4]
-    tmp = next(x for x in g.nodes if x[0] == int(lis[rowCount][1]))
-    beta2 = tmp[3]
-    lambda2 = tmp[4]
-    dist = 6378.388 * acos(sin(beta1) * sin(beta2) + cos(beta1) * cos(beta2) * cos(abs(lambda1 - lambda2)))
-    g.nodes[counter][2].append((int(row[1]), dist))
+        g.add_edge((int(row[0]),int(row[1])),dist)
+file.close()
+minLaengengrad = min( [x[3] for x in g.nodes.values()] )
+minBreitengrad = min([x[2] for x in g.nodes.values()])
+maxLaengengrad = max([x[3] for x in g.nodes.values()])
+maxBreitengrad = max([x[2] for x in g.nodes.values()])
+diffLaenge = maxLaengengrad-minLaengengrad
+diffBreite = maxBreitengrad-minBreitengrad
 
-#generic approach for inserting the edges (indepoendent of data layout)
-# with open("maps/edges.csv", newline="") as file:  # open file
-#     reader = csv.reader(file, delimiter=",")
-#     for row in reader:
-#         try:
-#             tmp = next(x for x in g.nodes if x[0] == int(row[0]))
-#             beta1 = tmp[3]
-#             lambda1 = tmp[4]
-#             tmp = next(x for x in g.nodes if x[0] == int(row[1]))
-#             beta2 = tmp[3]
-#             lambda2 = tmp[4]
-#             dist = 6378.388 * acos(sin(beta1) * sin(beta2) + cos(beta1) * cos(beta2) * cos(abs(lambda1 - lambda2)))
-#             g.add_edge((int(row[0]), int(row[1])), dist)
-#
-#         except:
-#             pass
+class Window(QMainWindow):
 
-#optimized approach with knowledge of data layout
+    def __init__(self):
+        super().__init__()
 
+        self.title = "PyQt5 Drawing Tutorial"
+
+        self.top = 150
+
+        self.left = 150
+
+        self.width = 1366
+
+        self.height = 768
+
+        self.InitWindow()
+
+        self.flag = False
+
+
+    def InitWindow(self):
+
+        self.setGeometry(self.top, self.left, self.width, self.height)
+        ###
+        painter = QtGui.QPainter(self)
+        self.pixmap = QtGui.QPixmap("maps/Black.jpg")
+        painter.drawPixmap(self.rect(), self.pixmap)
+        pen = QtGui.QPen(Qt.white, 2)
+        painter.setPen(pen)
+        for node in g.nodes:
+            x = ((maxLaengengrad - g.nodes[node][3]) / diffLaenge) * self.width
+            y = ((maxBreitengrad - g.nodes[node][2]) / diffBreite) * self.height
+            for edge in g.nodes[node][1]:
+                x1 = ((maxLaengengrad - g.nodes[edge[0]][3]) / diffLaenge) * self.width
+                y1 = ((maxBreitengrad - g.nodes[edge[0]][2]) / diffBreite) * self.height
+                pen = QtGui.QPen(Qt.red, 1)
+                painter.setPen(pen)
+                painter.drawLine(QPoint(x, y), QPoint(x1, y1))
+            pen = QtGui.QPen(Qt.white, 2)
+            painter.setPen(pen)
+            painter.drawPoint(x, y)
+        ###
+        self.show()
+
+    def paintEvent(self, event):
+        painter = QtGui.QPainter(self)
+        painter.drawPixmap(self.rect(), self.pixmap)
+        if self.flag:
+            pen = QtGui.QPen(Qt.yellow,3)
+            painter.setPen(pen)
+            x = self.mousePos.x()
+            y = self.mousePos.y()
+            min = 100000.0
+            key = 0
+            for i in g.nodes:
+                if sqrt(pow(g.nodes[i][3]-x,2)+pow(g.nodes[i][2]-y,2)) < min:
+                    min = sqrt(pow(g.nodes[i][3]-x,2)+pow(g.nodes[i][2]-y,2))
+                    key = i
+            painter.drawPoint(g.nodes[key][3],g.nodes[key][2])
+            self.show()
+
+    def mousePressEvent(self, event):
+        self.mousePos = event.pos()
+        self.flag = True
+        self.update()
+
+if __name__ =="__main__":
+    App = QApplication(sys.argv)
+    window = Window()
+    sys.exit(App.exec())
